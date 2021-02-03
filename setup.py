86facec662a1cf90f3d7be16079905a607c54d8c
AjayAru <ajay.arunachalam08@gmail.com>
2021-02-03 19:08:03 +0100

    typo fix

--- setup.py
+++ setup.py
@@ -1,34 +1,29 @@
 # Copyright (C) 2021-2022 Ajay Arunachalam <ajay.arunachalam08@gmail.com>
 # License: MIT, ajay.arunachalam08@gmail.com
 
-from setuptools import setup
-from msda.utils import version
+from distutils.core import setup
+import setuptools
+
+__version__ = '1.0.0'
 
 def readme():
-    with open('README.md','r', encoding='utf-8') as f:
-        README = f.read()
-    return README
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 
-setup(
-    name="msda",
-    version=f"{version()}",
-    description="MSDA - An open source, low-code time-series multi-sensor data analysis library in Python.",
-    long_description=readme(),
+setuptools.setup(
+    name='msda',
+    version=__version__,
+    description='MSDA - An open source, low-code time-series multi-sensor data analysis library in Python.',
+    long_description = readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ajayarunachalam/msda",
-    author="Ajay Arunachalam",
-    author_email="ajay.arunachalam08@gmail.com",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    packages=["msda"],
+    url='https://github.com/ajayarunachalam/msda',
+    install_requires=[
+          'matplotlib', 'numpy', 'datetime', 'pandas', 'statistics', 'IPython', 'ipywidgets',
+
+      ],
+    license='MIT',
     include_package_data=True,
-    install_requires=required
-)
+    author='Ajay Arunachalam',
+    author_email='ajay.arunachalam08@gmail.com',
+    )
